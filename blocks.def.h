2b53d7e8f2c476247e5e50a9763aca2b6e8b14a6
Daniel Soukhov <dsoukhov@gmail.com>
2021-08-25 12:02:51 -0400

    init t420s commit

--- blocks.def.h
+++ blocks.def.h
@@ -2,17 +2,18 @@
 //to access the signals use  pkill -RTMIN+<n> dwmblocks where n is defined below
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ /*use delim*/
-	{"VOL: ","pamixer --get-volume-human", 30, 1 ,0},
-  {"MIC: ","pamixer --source \"alsa_input.pci-0000_00_1f.3.analog-stereo\" --get-volume-human", 30, 2, 1},
-	{"CPU: ","/usr/lib/i3blocks/cpu_usage/cpu_usage", 5, 0 ,0},
-  {"", "cat /proc/cpuinfo|grep -i mhz|awk 'BEGIN {max = 0} {if ($4>max) max=$4} END {printf \"%dMhz\", max}'", 5, 0, 0},
-	{"","SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature/temperature", 5, 0 ,1},
-	{"","/usr/lib/i3blocks/iface/iface", 30, 0, 0},
-	{"","/usr/lib/i3blocks/bandwidth/bandwidth", 10, 0, 1},
-	{"ROOT: ","BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk/disk", 60, 0, 0},
-	{"HDD: ","BLOCK_INSTANCE=/mnt/hdd /usr/lib/i3blocks/disk/disk", 30, 0, 0},
-	{"HOME: ","BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk/disk", 60, 0, 1},
-	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0, 1},
+	{"VOL: ","pamixer --get-volume-human", 0, 1 ,0},
+	{"CPU: ","/usr/lib/i3blocks/cpu_usage/cpu_usage", 1, 0 ,0},
+        {"", "cat /proc/cpuinfo|grep -i mhz|awk 'BEGIN {max = 0} {if ($4>max) max=$4} END {printf \"%dMhz\", max}'", 1, 0, 0},
+	{"","/usr/lib/i3blocks/temperature/temperature", 5, 0 ,1},
+	//{"SSID:","/usr/lib/i3blocks/ssid/ssid", 5, 0, 0},
+	{"","/usr/lib/i3blocks/bandwidth/bandwidth", 5, 0, 1},
+	//{"ROOT: ","BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk/disk", 60, 0, 0},
+	//{"HOME: ","BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk/disk", 60, 0, 1},
+	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
+	{"BAT0: ", "BAT_NUMBER=0 /usr/lib/i3blocks/battery/battery", 5, 0, 0},
+	{"BAT1: ", "BAT_NUMBER=1 /usr/lib/i3blocks/battery/battery", 5, 0, 0},
+	{"WAT: " , "/home/daniel/scripts/wattage", 5, 0, 1},
 	{"", "date '+%a %d %b %Y %I:%M %p'", 10, 0, 1}
 };
 
