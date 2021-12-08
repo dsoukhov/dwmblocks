e860cdec18e7c35438b8b96014ea0bd40fc36b9a
Daniel Soukhov <dsoukhov@gmail.com>
2021-12-08 15:31:39 -0500

    replaced tabs w/ spaces

--- blocks.def.h
+++ blocks.def.h
@@ -1,20 +1,20 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 //to access the signals use  pkill -RTMIN+<n> dwmblocks where n is defined below
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ /*use delim*/
-	{"VOL: ","pamixer --get-volume-human", 0, 1 ,0},
+  /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ /*use delim*/
+  {"VOL: ","pamixer --get-volume-human", 0, 1 ,0},
   {"MIC: ","pamixer --source \"alsa_input.pci-0000_00_1f.3.analog-stereo\" --get-volume-human", 0, 2, 1},
-	{"CPU: ","/usr/lib/i3blocks/cpu_usage", 1, 0 ,0},
+  {"CPU: ","/usr/lib/i3blocks/cpu_usage", 1, 0 ,0},
   {"", "cat /proc/cpuinfo|grep -i mhz|awk 'BEGIN {max = 0} {if ($4>max) max=$4} END {printf \"%dMhz\", max}'", 1, 0, 0},
-	{"","SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature", 1, 0 ,1},
-	{"GPU: ","SENSOR_CHIP=radeon-pci-0100 /usr/lib/i3blocks/temperature", 1, 0 ,1},
-	//{"","/usr/lib/i3blocks/iface/iface", 10, 0, 0},
-	{"","/usr/lib/i3blocks/bandwidth", 1, 0, 1},
-	{"ROOT: ","BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
-	{"HDD: ","BLOCK_INSTANCE=/home/daniel/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
-	{"HOME: ","BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
-	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
-	{"", "date '+%a %d %b %Y %I:%M %p'", 1, 0, 1}
+  {"","SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature", 1, 0 ,1},
+  {"GPU: ","SENSOR_CHIP=radeon-pci-0100 /usr/lib/i3blocks/temperature", 1, 0 ,1},
+  //{"","/usr/lib/i3blocks/iface/iface", 10, 0, 0},
+  {"","/usr/lib/i3blocks/bandwidth", 1, 0, 1},
+  {"ROOT: ","BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
+  {"HDD: ","BLOCK_INSTANCE=/home/daniel/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
+  {"HOME: ","BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
+  {"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
+  {"", "date '+%a %d %b %Y %I:%M %p'", 1, 0, 1}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
