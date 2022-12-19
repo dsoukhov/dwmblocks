e7cf8ec27d8e87a4d114c747f678f3a5b1fa48cd
Daniel Soukhov <dsoukhov@gmail.com>
2022-12-18 21:21:14 -0500

    cleanup

--- blocks.def.h
+++ blocks.def.h
@@ -2,17 +2,17 @@
 //to access the signals use  pkill -RTMIN+<n> dwmblocks where n is defined below
 static const Block blocks[] = {
   /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ /*use delim*/
-  {"VOL: ","pamixer --get-volume-human", 0, 1 ,0},
-  {"MIC: ","~/scripts/mic vol", 0, 2, 1},
-  {"CPU: ","/usr/lib/i3blocks/cpu_usage", 1, 0 ,0},
+  {"VOL: ", "pamixer --get-volume-human", 0, 1, 0},
+  {"MIC: ", "~/scripts/mic vol", 0, 2, 1},
+  {"CPU: ", "/usr/lib/i3blocks/cpu_usage", 1, 0, 0},
   {"", "grep -i mhz /proc/cpuinfo|awk 'BEGIN {min = \"MAX\"} {if ($4<min) min=$4} END {printf \"%dMhz\", min}'", 1, 0, 0},
-  {"","SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature", 1, 0 ,1},
-  {"GPU: ","SENSOR_CHIP=radeon-pci-0100 /usr/lib/i3blocks/temperature", 1, 0 ,1},
-  //{"","/usr/lib/i3blocks/iface/iface", 10, 0, 0},
-  {"","/usr/lib/i3blocks/bandwidth", 5, 0, 1},
-  {"ROOT: ","BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
-  {"HDD: ","BLOCK_INSTANCE=/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
-  {"HOME: ","BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
+  {"", "SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature", 1, 0, 1},
+  {"GPU: ", "SENSOR_CHIP=amdgpu-pci-0100 /usr/lib/i3blocks/temperature", 1, 0, 0},
+  {"", "awk '{printf \"%dMhz\", $0/1000000}' /sys/class/drm/card0/device/hwmon/hwmon1/freq1_input", 1, 0, 1},
+  {"", "/usr/lib/i3blocks/bandwidth", 5, 0, 1},
+  {"ROOT: ", "BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
+  {"HDD: ", "BLOCK_INSTANCE=/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
+  {"HOME: ", "BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
   {"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
   {"", "date '+%a %d %b %Y %I:%M %p'", 1, 0, 1}
 };
