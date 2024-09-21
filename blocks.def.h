b924833b4efbd7b4be8e3a15a6ae51b0e393e2f8
Daniel Soukhov <dsoukhov@gmail.com>
2024-09-21 15:45:38 -0400

    compact

--- blocks.def.h
+++ blocks.def.h
@@ -2,22 +2,19 @@
 //to access the signals use  pkill -RTMIN+<n> dwmblocks where n is defined below
 static const Block blocks[] = {
   /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ /*use delim*/
-  {"VOL: ", "~/scripts/volume vol", 0, 1, 0},
-  {"MIC: ", "~/scripts/mic vol", 0, 2, 1},
-  {"CPU: ", "/usr/lib/i3blocks/cpu_usage", 1, 0, 0},
-  {"", "grep -i mhz /proc/cpuinfo|awk 'BEGIN {min = \"MAX\"} {if ($4<min) min=$4} END {printf \"%dMhz\", min}'", 1, 0, 0},
-  {"", "SENSOR_CHIP=coretemp-isa-0000 /usr/lib/i3blocks/temperature", 1, 0, 1},
-  {"GPU: ", "nvidia-smi --query-gpu=utilization.gpu --format=csv | awk 'NR==2{printf \"%d%\", $0}'", 1, 0, 0},
-  {"", "nvidia-smi --query-gpu=pstate --format=csv | awk 'NR==2{printf \"%s\", $0}'", 1, 0, 0},
-  {"", "nvidia-smi --query-gpu=memory.free --format=csv | awk 'NR==2{printf \"%dMiB\", $0}'", 1, 0, 0},
-  {"", "nvidia-smi --query-gpu=temperature.gpu --format=csv | awk 'NR==2{printf \"%d°C\", $0}'", 1, 0, 1},
-  {"", "/usr/lib/i3blocks/bandwidth", 5, 0, 1},
-  {"ROOT: ", "BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
-  {"HDD: ", "BLOCK_INSTANCE=/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
-  {"HOME: ", "BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
-  {"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
-  {"", "date '+%a %d %b %Y %I:%M %p'", 1, 0, 1}
+  {"", "~/scripts/volume vol", 0, 1, 0},
+  {"", "~/scripts/mic vol", 0, 2, 1},
+  {"", "/usr/lib/i3blocks/cpu_usage -d 0", 1, 0, 0},
+  {"", "grep -i mhz /proc/cpuinfo|awk 'BEGIN {min = \"MAX\"} {if ($4<min) min=$4} END {printf \"%.1fGhz\", min/1000}'", 1, 0, 0},
+  {"", "SENSOR_CHIP=coretemp-isa-0000 ~/scripts/temps", 1, 0, 1},
+  {"", "~/scripts/nvidia-gpu-info", 1, 0, 1},
+  {"", "/usr/lib/i3blocks/bandwidth -i ⬆ -o ⬇", 5, 0, 1},
+  {"/:", "BLOCK_INSTANCE=/ /usr/lib/i3blocks/disk", 10, 0, 0},
+  // {"HDD: ", "BLOCK_INSTANCE=/mnt/hdd /usr/lib/i3blocks/disk", 5, 0, 0},
+  {"~/:", "BLOCK_INSTANCE=/home /usr/lib/i3blocks/disk", 10, 0, 1},
+  {"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0, 1},
+  {"", "date '+%a %b %-d %-I:%M%p'", 1, 0, 1}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
